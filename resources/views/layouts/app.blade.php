5307500aa40cfa4e283d5d28ecef116b4cdd8e37
Anggit Utomo <pucukpisang97@gmail.com>
2024-03-26 00:59:14 +0700

    v0.1.4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
         <!-- Page Content -->
         <main>
             <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="w-full px-2 sm:max-w-7xl mx-auto sm:px-6 lg:px-8">
                     {{ $slot }}
                 </div>
             </div>
--- resources/views/livewire/transaction/index.blade.php
+++ resources/views/livewire/transaction/index.blade.php
@@ -11,23 +11,25 @@
         </x-primary-button-link>
     </div>
 
-    <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8 overflow-x-auto">
+    <div class="-my-2 sm:-mx-6 lg:-mx-8 overflow-x-auto">
         <div class="inline-block min-w-full py-2 aling-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded md:rounded-lg">
                 <table class="min-w-full divide-y divide-teal-800/10">
                     <thead class="bg-teal-800/10">
                         <tr>
-                            <th scope="col" class="py-3 pl-3 pr-3 text-center text-sm font-semibold text-gray-800">
+                            <th scope="col" class="py-3 sm:px-3 text-center text-sm font-semibold text-gray-800">
                                 {{ __('Transaction Name') }}
                             </th>
-                            <th scope="col" class="py-3 pl-3 pr-3 text-center text-sm font-semibold text-gray-800">
-                                {{ __('Transaction Date') }}
-                            </th>
-                            <th scope="col" class="py-3 pl-3 pr-3 text-center text-sm font-semibold text-gray-800">
+                            <th scope="col"
+                                class="hidden md:table-cell py-3 sm:px-3 text-center text-sm font-semibold text-gray-800">
                                 {{ __('Amount') }}
                             </th>
                             <th scope="col"
-                                class="py-3 pl-3 pr-3 text-left sm:text-center text-sm font-semibold text-gray-800">
+                                class="hidden sm:table-cell py-3 sm:px-3 text-center text-sm font-semibold text-gray-800">
+                                {{ __('Transaction Date') }}
+                            </th>
+                            <th scope="col"
+                                class="py-3 sm:px-3 text-center text-sm font-semibold text-gray-800">
                                 {{ __('Action') }}
                             </th>
                         </tr>
@@ -35,19 +37,29 @@
                     <tbody class="divide-y divide-gray-100 bg-white">
                         @forelse ($transactions as $transaction)
                             <tr>
-                                <td class="whitespace-nowrap text-center px-3 py-4 text-sm text-gray-800">
+                                <td class="whitespace-nowrap text-center sm:px-3 py-4 text-sm text-gray-800">
                                     <x-link :href="route('transaction.show', $transaction)">
                                         {{ $transaction->transaction_name }}
                                     </x-link>
+                                    <dl class="lg:hidden font-normal">
+                                        <dt class="sr-only md:hidden">{{ __('Transaction Amount') }}</dt>
+                                        <dd class="md:hidden text-gray-600">
+                                            {{ $transaction->local_currency }}
+                                        </dd>
+                                        <dt class="sr-only sm:hidden">{{ __('Transaction Date') }}</dt>
+                                        <dd class="text-gray-400 sm:text-gray-600 sm:hidden">
+                                            {{ $transaction->local_datetime_format }}
+                                        </dd>
+                                    </dl>
                                 </td>
-                                <td class="whitespace-nowrap text-center px-3 py-4 text-sm text-gray-800">
-                                    {{ \Carbon\Carbon::parse($transaction->transaction_date)->timezone($transaction->timezone)->format('d/m/Y H:i A') }}
+                                <td class="hidden md:table-cell whitespace-nowrap text-center sm:px-3 py-4 text-sm text-gray-800">
+                                    {{ $transaction->local_currency }}
                                 </td>
-                                <td class="whitespace-nowrap text-center px-3 py-4 text-sm text-gray-800">
-                                    {{ Number::currency($transaction->transaction_value, in: 'IDR') }}
+                                <td class="hidden sm:table-cell whitespace-nowrap text-center sm:px-3 py-4 text-sm text-gray-800">
+                                    {{ $transaction->local_datetime_format }}
                                 </td>
                                 <td
-                                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-left sm:text-center text-sm font-medium sm:pr-6">
+                                    class="relative whitespace-nowrap py-4 sm:px-3 text-center text-sm font-medium sm:pr-6">
                                     <x-link :href="route('transaction.edit', $transaction)">
                                         {{ __('Edit') }}
                                     </x-link>
